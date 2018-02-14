9b364af128850ea0aa3273e29d2b77332a1d43d0
Veronica Navarro <vnavarro138@gmail.com>
2018-02-13 19:02:27 -0800

    final just needs match code

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+  {
+    name: "Veronica",
+    photo: "https://vignette.wikia.nocookie.net/disney/images/c/ca/Snow_white_transparent.png/revision/latest?cb=20151111070659",
+    scores: [
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"    
+    ]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
